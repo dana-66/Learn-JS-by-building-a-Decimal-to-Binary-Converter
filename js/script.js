5fb4e2bdbcd5cfb745664eca105dd9aa0d024c82
dana-66 <danatarik66@gmail.com>
2024-06-28 07:56:35 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+// to check the value in the number input element whenever the user clicks the Convert button
+const checkUserInput = () => {};
